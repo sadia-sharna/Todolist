95f36939350ad391931e4f24c87a79d6cd7eb50f
SadiaGithub <s.sharna06@gmail.com>
2020-10-30 20:34:58 +0600

    Update todoList

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,11 +1,11 @@
 <template>
 <div>
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-4">
 
         </div>
-        <div class="col-md-6">
-            <div>
+        <div class="col-md-8">
+            <div style="position:fixed;width:35rem;">
 
                 <div class="input-group input-group-lg">
                     <div class="input-group-prepend">
@@ -13,31 +13,64 @@
                             <i class="fa fa-angle-down" aria-hidden="true"></i>
                         </span>
                     </div>
-                    <input type="text" class="form-control" placeholder="What needs to be done?" aria-label="Search" aria-describedby="basic-addon2" v-model="search" @input="showSearchItems = true" ref="searchBox" />
+                    <input type="text" class="form-control" placeholder="What needs to be done?" aria-label="Search" aria-describedby="basic-addon2" ref="searchBox" />
 
                 </div>
-                <ul class="list-group" v-if="filteredList.length > 0 && showSearchItems == true">
-                    <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" v-for="(item, index) in filteredList" :key="index" @click="
-                selectSearchItem(item);
-                showSearchItems = false;
-              " @mouseover="item.isMouseOveritem = true" @mouseleave="item.isMouseOveritem = false">
+                <ul class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
 
                         <!-- Filled-in rounded example -->
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input checkbox-round" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <div>
+                            <label style="font-size: 1.25rem;">task 1</label>
+                            <div class="round">
+
+                                <input type="checkbox" id="checkbox1" />
+                                <label for="checkbox1"></label>
+
+                            </div>
+
                         </div>
 
-                        <span class="badge badge-primary badge-pill" v-show="item.isMouseOveritem">
-                            Click to Select</span>
+                        <span>
+                            <i class="fa fa-close text-danger close"></i></span>
                     </a>
+                    <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+
+                        <div>
+                            <label style="font-size: 1.25rem;" class="del">task 2</label>
+                            <div class="round">
+
+                                <input type="checkbox" id="checkbox2" />
+                                <label for="checkbox2"></label>
+
+                            </div>
+
+                        </div>
+
+                        <span>
+                            <i class="fa fa-close text-danger close"></i></span>
+                    </a>
+                    <a class="list-group-item  d-flex justify-content-between align-items-center" style="padding: 0.3rem 0.25rem!important;">
+
+                        <div class="text-right ml-2"><small>1 items left</small></div>
+                        <div class="text-center">
+                            <div class="btn btn-padding border mr-1"><small>All</small></div>
+                            <div class="btn btn-padding border mr-1"><small>Active</small></div>
+                            <div class="btn btn-padding border"><small>Completed</small></div>
+                        </div>
+                        <div class="text-left btn u"><small>Clear Completed</small></div>
+
+                    </a>
+                    <a class="list-group-item  d-flex justify-content-between align-items-center ml-1 list-border-1"> </a>
+
+                    <a class="list-group-item  d-flex justify-content-between align-items-center ml-2 list-border-2"> </a>
+
                 </ul>
+
             </div>
         </div>
 
     </div>
-   
-    
 
 </div>
 </template>
@@ -46,96 +79,19 @@
 export default {
     data() {
         return {
-            exactMatch: false,
-            search: "",
-            selectedItem: "",
-            showSearchItems: false,
+
             isMouseOverList: false,
-            searchItemList: [{
-                    id: 1,
-                    name: "AngularJs",
-                    isMouseOveritem: false
-                },
-                {
-                    id: 2,
-                    name: "Node.Js",
-                    isMouseOveritem: false
-                },
-                {
-                    id: 3,
-                    name: "Vue.js",
-                    isMouseOveritem: false
-                },
-                {
-                    id: 4,
-                    name: "Bootstrap",
-                    isMouseOveritem: false
-                },
-                {
-                    id: 5,
-                    name: "CodeIgniter",
-                    isMouseOveritem: false
-                },
-                {
-                    id: 6,
-                    name: "ASP.NET",
-                    isMouseOveritem: false
-                },
-                {
-                    id: 7,
-                    name: "ASP.NET Core",
-                    isMouseOveritem: false
-                }
-            ]
+
         };
     },
-    computed: {
-        filteredList() {
-            if (this.exactMatch)
-                return this.searchItemList.filter((item) => {
-                    return item.name.toLowerCase().startsWith(this.search.toLowerCase());
-                });
-            else
-                return this.searchItemList.filter((item) => {
-                    return item.name.toLowerCase().includes(this.search.toLowerCase());
-                });
-        }
-    },
+
     methods: {
-        selectSearchItem(item) {
-            this.search = item.name;
-            this.selectedItem = item.name;
-            this.showSearchItems = false;
-        }
+
     }
 };
 </script>
 
 <style>
-/* .custom-control-input:focus~.custom-control-indicator {
-    -webkit-box-shadow: 0 0 0 1px #fff, 0 0 0 3px #0275d8;
-    box-shadow: 0 0 0 1px #fff, 0 0 0 3px #0275d8;
-    background-color:  black;
-}
-.custom-checkbox .custom-control-indicator {
-    border-radius: .55rem;
-} */
-.checkbox-round {
-    width: 1.3em;
-    height: 1.3em;
-    background-color: white;
-    border-radius: 50%;
-    vertical-align: middle;
-    border: 1px solid #ddd;
-    -webkit-appearance: none;
-    outline: none;
-    cursor: pointer;
-}
-
-.checkbox-round:checked {
-    background-color: gray;
-}
-
 .input-group-text {
     display: flex;
     align-items: center;
@@ -172,6 +128,8 @@ export default {
 
 .round {
     position: relative;
+    float: left;
+    margin-right: 1.6rem;
 }
 
 .round label {
@@ -179,41 +137,58 @@ export default {
     border: 1px solid #ccc;
     border-radius: 50%;
     cursor: pointer;
-    height: 28px;
-    left: 0;
+    height: 29px;
+    left: -12px;
     position: absolute;
-    top: 0;
+    top: 3px;
     width: 28px;
+
 }
 
 .round label:after {
-    border: 2px solid #fff;
+    border: 2px solid #19bb3e;
     border-top: none;
     border-right: none;
     content: "";
-    height: 6px;
-    left: 7px;
+    height: 8px;
+    left: 5px;
     opacity: 0;
     position: absolute;
-    top: 8px;
-    transform: rotate(-45deg);
-    width: 12px;
+    top: 7px;
+    transform: rotate(-57deg);
+    width: 15px;
+
 }
 
 .round input[type="checkbox"] {
     visibility: hidden;
 }
 
-.round input[type="checkbox"]:checked+label {
-    background-color: #66bb6a;
-    border-color: #66bb6a;
-}
-
 .round input[type="checkbox"]:checked+label:after {
     opacity: 1;
 }
 
-.containercheck {
-    margin: 0 auto;
+.btn-padding {
+    padding: 0.175rem 0.55rem !important;
+    line-height: 1.5 !important;
+}
+
+.del {
+    text-decoration: line-through;
+    color: silver;
+}
+
+.u {
+    text-decoration: underline;
+}
+.list-border-1{
+    padding: 0.16rem 0.1rem!important;width:98%
+}
+.list-border-2{
+    padding: 0.16rem 0.1rem!important;width:97%
+}
+.close{
+    color:red;
+    font-size:1.2rem; 
 }
 </style>
